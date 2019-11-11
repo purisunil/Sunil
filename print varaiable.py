0b625a5d18c562e5acd1158970f722c6c3caaa73
Sunil Puri <purisunil579@gmail.com>
2019-11-11 17:23:24 +0545

    vary

---
+++ print varaiable.py
@@ -0,0 +1,13 @@
+name=input("enter name")
+age=input("enter age")
+print(" enter two number")
+num1=int(input("first num"))
+num2=int(input("second num"))
+sum=num1+num2
+product=num1*num2
+difference=num2-num1
+division=num2/num1
+print("the sum of numbers is", int(sum))
+print ("the product of numbers is",int(product))
+print ("the differnce of num is",int(difference))
+print("the division of num is ",float(division))
